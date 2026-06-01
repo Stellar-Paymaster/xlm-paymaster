f747786d8f298ebfd85435dbaa0e6b3ed8c6a70a
Doris Maduegbunam <dorismaduegbunam@gmail.com>
2026-05-31 23:52:45 -0700

    feat: interactive-congestion-fee-estimator-graph

--- admin-dashboard/src/fees/index.ts
+++ admin-dashboard/src/fees/index.ts
@@ -1,4 +1,5 @@
 export { FeeEstimationWidget } from "./components/FeeEstimationWidget";
+export { CongestionFeeEstimatorGraph } from "./components/CongestionFeeEstimatorGraph";
 export { LocalizedFeeEstimator, estimateFeeByRegion } from "./LocalizedFeeEstimator";
 export { getCongestionConfig, getCongestionMultiplier, GLOBAL_AVERAGE_MULTIPLIER } from "./config/region-congestion-config";
 export type { FeeEstimationResult, CongestionLevel, CongestionConfig } from "./types";
